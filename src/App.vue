7b5df891cafd7c666ba196663c387d71992fcaae
Abdukadir Samikjanov <samikjanovabdukadr@gmail.com>
2023-01-07 16:24:43 +0500

    Start project

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container w-[100vw] h-[100vh] flex justify-center items-center">
+    <div class="w-[500px] h-[400px] shadow-2xl relative border-2 border-rose-500 rounded-3xl">
+      <h1 class="text-4xl text-gray-500 absolute top-[40px] left-1/3 right-1/3">Dictionary</h1>
+      <div class="w-full h-full pt-[110px] flex justify-evenly items-start">
+        <TheWord v-for="item in wordBase" :key="item" :word="item.spelling" :translate="item.translate" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TheWord from './components/TheWord.vue';
+export default {
+  components: {
+    TheWord
+  },
+
+  data() {
+    return {
+      wordBase: [
+        {
+          spelling: 'hello',
+          translate: 'привет',
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TheWord.vue
@@ -0,0 +1,20 @@
+<template>
+  <p class="text-gray-400 text-4xl">
+    {{ word }}
+  </p>
+
+  <span class="text-gray-400 text-4xl">
+    {{ translate }}
+  </span>
+
+</template>
+
+<script>
+export default {
+  props: {
+    word: String,
+    translate: String
+  }
+}
+</script>
+
